e6580de62823a5b4bf38f5527161f2415c14428e
puntoCL <Puntocl19@gmail.com>
2018-09-30 03:15:52 -0300

    Delete firstCheck.php

--- var/www/html/firstCheck.php
+++
@@ -1,7 +0,0 @@
-<?php 
-// ejecutando el archivo y obteniendo la salida
-
-exec("sudo python /var/www/html/firstCheck.py " ,$output);
-
-// la salida está codificada en json
-echo json_encode($output); 
