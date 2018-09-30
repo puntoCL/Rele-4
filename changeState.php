6c133aec46c9ff603e3f096b2648b0e63eb3ea9e
puntoCL <Puntocl19@gmail.com>
2018-09-29 22:30:01 -0300

    Update changeState.php

--- changeState.php
+++ changeState.php
@@ -6,10 +6,10 @@ if(isset($_POST['clicked'])){
 	if($_POST['clicked']  == 'true' ){
 		// ejecutando el comando : sudo python relay_on.py id
 		// donde el id es la cantidad de relevo que queremos encender / apagar
-		exec("sudo python /var/www/html/relepi/relay_on.py " . $_POST['relayId']);
+		exec("sudo python /var/www/html/Rele-4/relay_on.py " . $_POST['relayId']);
 		echo "true";
 	}else{
-		exec("sudo python /var/www/html/relepi/relay_off.py " . $_POST['relayId']);
+		exec("sudo python /var/www/html/Rele-4/relay_off.py " . $_POST['relayId']);
 		echo "false";
 	}
 }
