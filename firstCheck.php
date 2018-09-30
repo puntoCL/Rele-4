fb2985910803401ac648cec0269076a0f27a90c7
puntoCL <Puntocl19@gmail.com>
2018-09-29 22:30:27 -0300

    Update firstCheck.php

--- firstCheck.php
+++ firstCheck.php
@@ -2,7 +2,7 @@
 // ejecutando el archivo y obteniendo la salida 
 // http://php.net/manual/en/function.exec.php
 
-exec("sudo python /var/www/html/relepi/firstCheck.py " ,$output);
+exec("sudo python /var/www/html/Rele-4/firstCheck.py " ,$output);
 
 // la salida está codificada en json
 echo json_encode($output); 
