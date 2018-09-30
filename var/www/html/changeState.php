ee5e046be28b72af967d7cb4947ccb73726c333a
puntoCL <Puntocl19@gmail.com>
2018-09-30 03:00:54 -0300

    Update changeState.php

--- var/www/html/changeState.php
+++ var/www/html/changeState.php
@@ -6,10 +6,10 @@ if(isset($_POST['clicked'])){
 	if($_POST['clicked']  == 'true' ){
 			// ejecutando el comando: sudo python relay_on.py id
 			// donde el id es la cantidad de relevo que queremos activar / desactivar
-		exec("sudo python /home/pi/relay_on.py " . $_POST['relayId']);
+		exec("sudo python /var/www/html/relay_on.py " . $_POST['relayId']);
 		echo "true";
 	}else{
-		exec("sudo python /home/pi/relay_off.py " . $_POST['relayId']);
+		exec("sudo python /var/www/html/relay_off.py " . $_POST['relayId']);
 		echo "false";
 	}
 }
