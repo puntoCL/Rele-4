f50d84bea5118c8b9eac52d5cd1d901dc3a2f6be
puntoCL <Puntocl19@gmail.com>
2018-09-29 21:22:54 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,19 +1,19 @@
-# obteniendo la biblioteca principal de GPIO
+#obteniendo la biblioteca principal de GPIO
 import RPi.GPIO as GPIO
-# obtener la biblioteca de tiempo
+#obtener la biblioteca de tiempo
 import time
 
 # configurando un modo actual
 GPIO.setmode(GPIO.BCM)
-#eliminando los warings 
+#eliminando los warings
 GPIO.setwarnings(False)
-#creando una lista (matriz) con el número de GPIO que usamos
+#creando una lista (matriz) con el numero de GPIO que usamos
 pins = [18,17,15,14] 
 
-#estableciendo el modo para todos los pines para que todos estén encendidos 
+#estableciendo el modo para todos los pines para que todos esten encendidos 
 GPIO.setup(pins, GPIO.OUT)
 
-#bucle de pines = 18 sigiendo 17 ,15, 14 
+#bucle de pines = 18 sigiendo 17 ,15, 14
 for pin in pins :
 	#configurando el GPIO en ALTO o 1 o verdadero
 	GPIO.output(pin,  GPIO.HIGH)
@@ -24,7 +24,7 @@ for pin in pins :
 	#Espera de 0,5 segundos
 	time.sleep(0.5)
 
-	#Verificar si el relé actual se está ejecutando e imprimirlo 
+	#Verificar si el rele actual se esta ejecutando e imprimirlo
 	if not GPIO.input(pin) : 
 		print("Pin "+str(pin)+" Esta trabajando" )
 		
