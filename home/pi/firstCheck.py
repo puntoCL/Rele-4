37786c036527d2a1ca17294fa5da25ac8a823143
puntoCL <Puntocl19@gmail.com>
2018-09-30 02:46:50 -0300

    Update firstCheck.py

--- home/pi/firstCheck.py
+++ home/pi/firstCheck.py
@@ -4,27 +4,27 @@
 
 
 
-#getting the main GPIO libraly
+#obtener la biblioteca principal de GPIO
 import RPi.GPIO as GPIO
 #time and json
 import time
 import json
 
-# setting a list of used pins 
+# establecer una lista de pines usados 
 pins = [18,17,15,14]
 
-# setting GPIO mode
+# configurando el modo GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-# create empty list 
+# crear lista vacia
 arr = []
-# for loop from 0 to 3 
+# para el bucle de 0 a 3
 for x in range(0,4) : 
-	#putting all relays in on state if GPIO is not setup
+	#poner todos los reles en estado si GPIO no esta configurado
 	GPIO.setup(pins[x],GPIO.OUT)
 	#putting the relay state in the empty list 
 	arr.append(not GPIO.input(pins[x]))
 
-# printing the list of state's in json format
+# imprimiendo la lista de estados en formato json
 print json.dumps({0:arr[0],1:arr[1],2:arr[2],3:arr[3]})
