93a26408f3c5978a5d787eb217fc5c129b7b0bfb
puntoCL <Puntocl19@gmail.com>
2018-09-30 02:51:08 -0300

    Update changeState.php

--- var/www/html/changeState.php
+++ var/www/html/changeState.php
@@ -1,11 +1,11 @@
 <?php 
 // http://php.net/manual/en/function.exec.php
-// if we have POST value named "clicked"
+// si tenemos el valor POST llamado "clicked"
 if(isset($_POST['clicked'])){
 
 	if($_POST['clicked']  == 'true' ){
-		// executing the command : sudo python relay_on.py id
-		// where the id is the number of relay that we want to switch on/off
+			// ejecutando el comando: sudo python relay_on.py id
+			// donde el id es la cantidad de relevo que queremos activar / desactivar
 		exec("sudo python /home/pi/relay_on.py " . $_POST['relayId']);
 		echo "true";
 	}else{
